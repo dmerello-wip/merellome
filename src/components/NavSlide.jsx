92c98da35622e7d311b932facb21f5e69b394a61
Diego Merello <diego@wipitalia.it>
2024-10-06 16:46:09 +0200

    text animation

--- src/components/NavSlide.jsx
+++ src/components/NavSlide.jsx
@@ -17,11 +17,6 @@ const NavSlide = (props) => {
   useGSAP((context, contextSafe) => {
     let ctx = gsap.context(() => {
       
-        const trigger = {
-            trigger: slideRef.current,
-            scrub: 0.1,
-            start: 'top top',
-        }
         const controlledRotation = cameraStartSettings;
         
         gsap.to(controlledRotation, {
@@ -31,12 +26,41 @@ const NavSlide = (props) => {
           positionX: cameraEndSettings.positionX,
           positionY: cameraEndSettings.positionY,
           positionZ: cameraEndSettings.positionZ,
-          scrollTrigger: trigger,
+          scrollTrigger: {
+              trigger: slideRef.current,
+              scrub: 0.1,
+              start: 'top top',
+          },
           ease: "power1.inOut",
-
           onUpdate: () => {
               setCamera(controlledRotation)
           }
+        });
+
+        gsap.from(`#${id} .letter`, {
+          opacity: 0,
+          filter: 'blur(10px)',
+          scrollTrigger: {
+              trigger: slideRef.current,
+              scrub: 0.1,
+              start: 'top 30%',
+              end: 'top top',
+          },
+          stagger: {
+              each: 0.04,
+          },
+        });
+
+
+        gsap.from(`#${id} .navSlide__content__description`, {
+          opacity: 0,
+          y: 0,
+          scrollTrigger: {
+              trigger: slideRef.current,
+              scrub: 0.1,
+              start: 'top 5%',
+              end: 'top top',
+          },
         });
     });
 
@@ -44,11 +68,19 @@ const NavSlide = (props) => {
 
   }, [cameraStartSettings, cameraEndSettings, id]);
 
+  const renderSplitByLetter = str => {
+      const words = str.split(' ');
+      return words.map((el, i) => {
+          const letters = el.split('').map((el, i) => <span className="letter" key={`letter-${i}`}>{el}</span>);
+          return <span className="word" key={`word-${i}`}>{letters} </span>;
+      });
+  };
+
   return (
     <div className="navSlide" ref={slideRef} id={id}>
       <div className='navSlide__content'>
         <div className='navSlide__content__title'>
-          <h2>{title}</h2>
+          <h2>{renderSplitByLetter(title)}</h2>
         </div>
         <div className='navSlide__content__description' dangerouslySetInnerHTML={{ __html: description }} />
         {children}
