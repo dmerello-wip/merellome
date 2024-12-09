977072944d268a2c960322266b8787b06ddc53aa
Diego Merello <diego@wipitalia.it>
2024-12-09 21:36:43 +0100

    package-lock

--- app/page.jsx
+++ app/page.jsx
@@ -2,29 +2,31 @@ import '@/helpers/envConfig'
 import { Header } from "@/components/Header"
 import { Navigator } from "@/components/navigator/Navigator"
 import { TempSpacer } from "@/components/TempSpacer"
-// import { Suspense } from 'react';
-// import { Loader } from '@/components/Loader'
 import { ArticlesSlider } from '@/components/ArticlesSlider'
-
+// import slidesContents from '@/db/slides.json';
+// import articlesContents from '@/db/articles.json';
 
 export default async function Page() {
+
+  const url = process.env.BASE_URL || '';
   
-  const slides = await fetch(`${process.env.BASE_URL}/api/navigator`, { next: { tags: ['navigator'] } }).then((res) =>
+  const slides = await fetch(`${url}/api/navigator`, { next: { tags: ['navigator'] } }).then((res) =>
     res.json()
   )
-  
-  const articles = await fetch(`${process.env.BASE_URL}/api/articles`, { next: { tags: ['articles'] } }).then((res) =>
-    res.json()
+  const articles = await fetch(`${url}/api/articles`, { next: { tags: ['articles'] } }).then((res) =>
+    res.json(
   )
+
+  // yes, using the api is useless... why don't:
+  // const slides = { slides: slidesContents.slides };
+  // const articles = { items: articlesContents.articles };
 
   return (
     <>
       <Header />
 
       
-      {/* <Suspense fallback={<Loader fixed={true} />}> */}
-        <Navigator contents={slides} />
-      {/* </Suspense>  */}
+      <Navigator contents={slides} />
      
       <ArticlesSlider contents={articles.items} />
       <TempSpacer>1. Contenuto sotto al navigator</TempSpacer>
