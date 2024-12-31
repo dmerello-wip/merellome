57d677a729105e3f9483b39d037c05c75a60516e
Diego Merello <diego@wipitalia.it>
2024-12-31 17:47:01 +0100

    testing

--- src/components/PinTabs.jsx
+++ src/components/PinTabs.jsx
@@ -1,6 +1,8 @@
 import _PinTabs from '@/styles/components/PinTabs.scss'
 import _Moire from '@/styles/components/Moire.scss'
 import _Grid from '@/styles/basics/Grids.scss'
+import _Typo from '@/styles/basics/Typography.scss'
+import Image from 'next/image'
 
 const PinTabs = () => {
 
@@ -12,17 +14,25 @@ return (
             <em>01.</em> Discover
           </h2>
           <div className="PinTabs__tab__content">
-            <div className="grid-3x3">
-              <div className='grid-3x3__item'></div>
-              <div className='grid-3x3__item'></div>
-              <div className='grid-3x3__item'></div>
-              <div className='grid-3x3__item'></div>
-              <div className='grid-3x3__item'></div>
-              <div className='grid-3x3__item'></div>
+            <div className="gr3x3">
+              <div className='gr3x3__item gr--1-lg'>
+                <p className='txt-lg'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
+              </div>
+              <div className='gr3x3__item gr--2-md'>
+                <Image
+                  src="/img/elisava.jpg"
+                  alt="Elisava"
+                  fill
+                  style={{ width: '100%', height: '100%', objectFit:"cover" }} 
+                />
+              </div>
+              <div className='gr3x3__item'>
+                <p>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here. </p>
+              </div>
               <div
-                className='grid-3x3__item moire__text'
+                className='gr3x3__item moire__text'
                   style={{gridRow: '1/4', gridColumn: '3/4'}}>
-                    {[...Array(200)].map((_, i) => <span key={`moire_text_1_${i}`}>design is... </span>)}
+                    {[...Array(200)].map((_, i) => <span key={`moire_text_1_${i}`}>Design </span>)}
               </div>
             </div>
           </div>
@@ -32,14 +42,14 @@ return (
             <em>02.</em> Map
           </h2>
           <div className="PinTabs__tab__content">
-            <div className="grid-3x3">
-              <div className='grid-3x3__item'></div>
-              <div className='grid-3x3__item'></div>
-              <div className='grid-3x3__item'></div>
-              <div className='grid-3x3__item'></div>
-              <div className='grid-3x3__item'></div>
-              <div className='grid-3x3__item'></div>
-              <div className="grid-3x3__item moire__diag-r-lines"
+            <div className="gr3x3">
+              <div className='gr3x3__item'></div>
+              <div className='gr3x3__item'></div>
+              <div className='gr3x3__item'></div>
+              <div className='gr3x3__item'></div>
+              <div className='gr3x3__item'></div>
+              <div className='gr3x3__item'></div>
+              <div className="gr3x3__item moire__diag-r-lines"
                     style={{gridRow: '1/4', gridColumn: '3/4'}}>
               </div>
             </div>
@@ -50,14 +60,14 @@ return (
             <em>03.</em> Explore
           </h2>
           <div className="PinTabs__tab__content">
-            <div className="grid-3x3">
-              <div className='grid-3x3__item'></div>
-              <div className='grid-3x3__item'></div>
-              <div className='grid-3x3__item'></div>
-              <div className='grid-3x3__item'></div>
-              <div className='grid-3x3__item'></div>
-              <div className='grid-3x3__item'></div>
-              <div className="grid-3x3__item moire__diag-l-lines"
+            <div className="gr3x3">
+              <div className='gr3x3__item'></div>
+              <div className='gr3x3__item'></div>
+              <div className='gr3x3__item'></div>
+              <div className='gr3x3__item'></div>
+              <div className='gr3x3__item'></div>
+              <div className='gr3x3__item'></div>
+              <div className="gr3x3__item moire__diag-l-lines"
                     style={{gridRow: '1/4', gridColumn: '3/4'}}>
               </div>
             </div>
