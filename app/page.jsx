d2ea07e3fdf286b4c491df0497d9677f75bb3113
Diego Merello <diego@wipitalia.it>
2025-01-05 21:24:07 +0100

    smartphone responsiveness pins

--- app/page.jsx
+++ app/page.jsx
@@ -4,28 +4,28 @@ import { Navigator } from "@/components/navigator/Navigator"
 import { ArticlesSlider } from '@/components/ArticlesSlider'
 import { TempSpacer } from "@/components/TempSpacer"
 import { PinTabs } from "@/components/PinTabs/PinTabs"
-// import slidesData from '@/db/slides.json';
-// import articlesData from '@/db/articles.json';
-// import methodData from '@/db/method.json';
+import slidesData from '@/db/slides.json';
+import articlesData from '@/db/articles.json';
+import methodData from '@/db/method.json';
 
 export default async function Page() {
 
   const url = process.env.BASE_URL || 'https://merellome.vercel.app' || 'http://localhost:3000';
   
-  const slides = await fetch(`${url}/api/navigator`, { next: { tags: ['navigator'] } }).then((res) =>
-    res.json()
-  )
-  const articles = await fetch(`${url}/api/articles`, { next: { tags: ['articles'] } }).then((res) =>
-    res.json()
-  )
-  const pins = await fetch(`${url}/api/method`, { next: { tags: ['method'] } }).then((res) =>
-    res.json()
-  )
+  // const slides = await fetch(`${url}/api/navigator`, { next: { tags: ['navigator'] } }).then((res) =>
+  //   res.json()
+  // )
+  // const articles = await fetch(`${url}/api/articles`, { next: { tags: ['articles'] } }).then((res) =>
+  //   res.json()
+  // )
+  // const pins = await fetch(`${url}/api/method`, { next: { tags: ['method'] } }).then((res) =>
+  //   res.json()
+  // )
 
   // yes, using the api is useless... i can simply import the json files:
-  // const slides = { slides: slidesData.slides };
-  // const articles = { items: articlesData.articles };
-  // const pins = { items: methodData.pins };
+  const slides = { slides: slidesData.slides };
+  const articles = { items: articlesData.articles };
+  const pins = { items: methodData.pins };
 
   return (
     <>
